94dcde7820f6dbf69e3ee8fab873ffba64df7025
iLGZH <dearmengqiyuan@163.com>
2023-08-15 22:38:36 +0800

    add deconstructing option

---
+++ deconstructing_option/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = Some(5);
+    plus_one(x);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
